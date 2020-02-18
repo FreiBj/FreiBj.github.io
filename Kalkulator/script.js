28df207a3d2b5f855c88541e98aaf95c8b374a98
pcvasusgaming50 <frei.bjerkestrand@hotmail.no>
2020-02-18 09:39:12 +0100

    Changed color on input field

--- Kalkulator/script.js
+++ Kalkulator/script.js
@@ -1,8 +1,9 @@
 var frei = ["Frei", 30];
+var emil = ["Emil", 10]
 
 var okning;
 
-function myFunction() {
+function myFunction(name) {
   console.log("myFunction is working!");
   console.log(frei);
   frei[1] = frei[1]+ 10
