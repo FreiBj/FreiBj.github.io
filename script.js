c0a1f2dcbec40135667624b392e8ba00af41861b
Frei Bjerkestrand <frei.bjerkestrand@hotmail.no>
2020-03-12 23:22:23 +0100

    New layout for landing page

---
+++ script.js
@@ -0,0 +1,98 @@
+// function pageTransition() {
+  
+//   alert("PageTransition active");
+//   var tl = gsap.timeline();
+  
+//   tl.to("ul.transition li", {duration:.5, scaleY:1, transformOrigin: "bottom left", stagger: .2})
+//   tl.to("ul.transition li", {duration:.5, scaleY:0, transformOrigin:"bottom left", stagger: .1, delay: .1})
+  
+// }
+
+// Function to add and remove the page transition screen
+function pageTransition() {
+
+  var tl = gsap.timeline();
+  tl.set('.loading-screen', { transformOrigin: "bottom left"});
+  tl.to('.loading-screen', { duration: .5, scaleY: 1});
+  tl.to('.loading-screen', { duration: .5, scaleY: 0, skewX: 0, transformOrigin: "top left", ease: "power1.out", delay: 1 });
+}
+
+function contentAnimation() {
+  // alert("Contant animation");
+  var tl = gsap.timeline();
+  tl.from(".left", {duration:1.5, translateY:50,opacity:0})
+  tl.to("img", {clipPath:"polygon(0 0, 100% 0, 100% 100%, 0 100%)"})
+  
+}
+
+
+function delay(n) {
+  n = n || 2000;
+  return new Promise(done => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
+}
+
+
+barba.init({
+  
+  sync: true,
+  
+  transitions: [{
+    async leave(data) {
+      
+      const done = this.async();
+      
+      pageTransition();
+      await delay(1500);
+      done();
+    },
+    
+    async beforeEnter(data) {
+      pageTransition();
+    },
+    
+    async enter(data) {
+      contentAnimation();
+    },
+    
+    async once(data) {
+      contentAnimation();
+    }
+    
+  }]
+  
+});
+
+$(function() {
+
+  barba.init({
+
+    sync: true,
+
+    transitions: [{
+
+      async leave(data) {
+        
+        const done = this.async();
+        
+        pageTransition();
+        await delay(1000);
+        done();
+
+      },
+
+      async enter(data) {
+        contentAnimation();
+      },
+
+      async once(data) {
+        contentAnimation();
+      }
+
+    }]
+  });
+
+});
