d77b0b3438b669a6ed2487909da5214796878e77
FreiBj <frei.bjerkestrand@hotmail.no>
2020-05-17 01:27:31 +0200

    Animations. corrections

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -1,16 +1,20 @@
  // portfolio
- $('.gallery ul li a').click(function() {
+ $('.gallery ul li a').click(function () {
+     $('.gallery ul li').removeClass('animate__animated animate__fadeIn');
      var itemID = $(this).attr('href');
      $('.gallery ul').addClass('item_open');
+     $('.gallery ul li').addClass('animate__animated animate__fadeOut');
      $(itemID).addClass('item_open');
      return false;
  });
- $('.close').click(function() {
+ $('.close').click(function () {
      $('.port, .gallery ul').removeClass('item_open');
+     $('.gallery ul li').removeClass('animate__animated animate__fadeOut');
+     $('.gallery ul li').addClass('animate__animated animate__fadeIn');
      return false;
  });
 
- $(".gallery ul li a").click(function() {
+ $(".gallery ul li a").click(function () {
      $('html, body').animate({
          scrollTop: parseInt($("#top").offset().top)
      }, 400);
