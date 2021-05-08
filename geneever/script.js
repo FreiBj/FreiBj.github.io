1cfd511de3b5f5788ee99042a2f748f54f7dfbd9
Frei Bjerkestrand <frei.bjerkestrand@hotmail.no>
2021-05-08 11:01:08 +0200

    Idk

--- geneever/script.js
+++ geneever/script.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
+
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("open");
     links.forEach(link => {
